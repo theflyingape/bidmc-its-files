c198402045c73c26a1e5b133791d51bac22de695
Robert Hurst <theflyingape@gmail.com>
2019-11-07 17:35:46 -0500

    watch to convert PCL files

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ⚕️ BIDMC ITS: Files
 
 A Node.js express server providing a REST API backend to its filesystem watcher.
+
+``` bash
+# install package dependencies
+$ sudo yum install ghostscript libgs-devel
+
+# --GS4JS_LIB="libgs.so"
+$ npm install ghostscript4js --GS4JS_HOME="/usr/lib64" --save
+```
--- src/main.ts
+++ src/main.ts
@@ -6,30 +6,63 @@ import chokidar = require('chokidar')
 import dns = require('dns')
 import express = require('express')
 import fs = require('fs')
+import gs = require('ghostscript4js')
 import https = require('https')
 import os = require('os')
-import path = require('path')
 //import pty = require('node-pty')
+import path = require('path')
 import serverStatic = require('serve-static')
 import syslog = require('modern-syslog')
 //import ws = require('ws')
+import { ChildProcess, spawn } from 'child_process'
 const { URL } = require('url')
 
 process.title = 'watcher'
-process.chdir(__dirname)
+//process.chdir(__dirname)
 
 let loglevel = 0 || 'LOG_NOTICE'
 if (isNaN(+loglevel)) loglevel = syslog.level[loglevel]
 
 syslog.open(process.title)
 syslog.upto(+loglevel)
-syslog.note(`${process.title} started`)
+syslog.note(`started using ${gs.version().product}`)
 
 let events = 0
 
-chokidar.watch('.').on('all', (event, path) => {
-    console.log(event, path)
+const watcher = chokidar.watch('.', {
+    ignoreInitial: true
+})
+
+syslog.note(`watching for PCL files`)
+console.log(watcher.getWatched())
+
+watcher.on('add', (file) => {
+    console.log(event, file)
     events++
+
+    if (/.*\.pcl$/i.test(file)) {
+        let pdf = path.join(path.dirname(file), path.basename(file, path.extname(file)) + '.pdf')
+
+        const child: ChildProcess = spawn('./gpcl6',
+            ['-dNOPAUSE', `-sOutputFile=${pdf}`, '-sDEVICE=pdfwrite', `${file}`])
+            .on('exit', (code, signal) => {
+                syslog.note(`${pdf} exit code ${code} ${signal && 'signal ' + signal || ''}`)
+            })
+
+/*
+        gs.execute(`-dNOPAUSE -sOutputFile=${pdf} -sDEVICE=pdfwrite ${path}`)
+            .then(() => {
+                console.log(`converted to ${pdf}`)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+*/
+        }
+    })
+
+watcher.on('ready', () => {
+    console.log('Initial scan complete. Ready for changes')
 })
 
 process.on('SIGHUP', function () {
@@ -43,8 +76,9 @@ process.on('SIGINT', function () {
 })
 
 process.on('SIGQUIT', function () {
-    console.log(new Date() + ' :: received quit')
+    console.log(new Date() + ' shutdown - ', events, 'trapped')
     syslog.warn('quit')
+    process.exit()
 })
 
 process.on('SIGTERM', function () {
